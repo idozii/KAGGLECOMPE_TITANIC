7b5c27aec9415cac29315b1fc316b3070359b6e2
idozii <tue.nguyenpc0401@hcmut.edu.vn>
2025-03-20 10:33:29 +0700

    update

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split 
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import OneHotEncoder
-from sklearn.ensemble import RandomForestClassifier  # Changed from Regressor to Classifier
+from sklearn.ensemble import RandomForestClassifier 
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import accuracy_score, classification_report  
